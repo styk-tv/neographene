928293ac24ac654d89ff55f4e233e95d4243c4e2
Pablo Cabezas <pabcabsal@gmail.com>
2017-10-09 18:03:04 +0200

    Try things

--- neographene/neographene.py
+++ neographene/neographene.py
@@ -14,12 +14,29 @@ neomodel_to_graphene_dict = {k: v for k, v in zip(neomodel_types, graphene_types
 graphene_to_neomodel_dict = {k: v for k, v in zip(graphene_types, neomodel_types)}  # graphene -> neomodel
 
 
-class SchemaFactory(ObjectType):
-    def __init__(self, neomodel: StructuredNode):
-        super(SchemaFactory, self).__init__()
-        print(neomodel.__dict__.items())
-        for field_name, field_instance in neomodel.__dict__.items():
-            setattr(self, field_name, neomodel_to_graphene_dict[field_instance.__class__])
+def ClassFactory(name, arg_names, BaseClass):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            # here, the argnames variable is the one passed to the
+            # ClassFactory call
+            if key not in arg_names:
+                raise TypeError("Argument %s not valid for %s"
+                                % (key, self.__class__.__name__))
+            setattr(self, key, value)
+        BaseClass.__init__(self, name[:-len("Class")])
+
+    newclass = type(name, (BaseClass,), {"__init__": __init__})
+    return newclass
+
+#
+# class SchemaFactory(ObjectType):
+#     def __init__(self, neomodel: StructuredNode):
+#         super(SchemaFactory, self).__init__()
+#         print(neomodel.__dict__.items())
+#         for field_name, field_instance in neomodel.__dict__.items():
+#             setattr(self, field_name, neomodel_to_graphene_dict[field_instance.__class__])
+def SchemaFactory(neomodel):
+    return ClassFactory(ObjectType, neomodel.__dict__.items())
 
 
 class NodeFactory(StructuredNode):
