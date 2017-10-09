8ef562d3d4c8dcc641d1bc869344f6bbaf900039
kingoodie <pabcabsal@gmail.com>
2017-10-09 21:01:09 +0200

    Clean

--- neographene/neographene.py
+++ neographene/neographene.py
@@ -13,13 +13,39 @@ neomodel_to_graphene_dict = {k: v for k, v in zip(neomodel_types, graphene_types
 graphene_to_neomodel_dict = {k: v for k, v in zip(graphene_types, neomodel_types)}  # graphene -> neomodel
 
 
-class SchemaFactory(graphene.ObjectType, object):
+class SchemaFactory(graphene.ObjectType):
     def __init__(self, neomodel: StructuredNode):
         for field_name, field_instance in neomodel.__dict__.items():
             setattr(self, field_name, neomodel_to_graphene_dict[field_instance])
 
+    def get_schema(self):
+        return self
 
-class NodeFactory(neomodel.StructuredNode, object):
+
+class NodeFactory(neomodel.StructuredNode):
     def __init__(self, schema: graphene.ObjectType):
         for field_name, field_instance in schema.__dict__.items():
             setattr(self, field_name, graphene_to_neomodel_dict[field_instance])
+
+    def get_node(self):
+        return self
+
+
+class TestSchema(graphene.ObjectType):
+    def __init__(self):
+        for index, type in enumerate(graphene_types):
+            setattr(self, "field" + str(index), type)
+
+
+class TestNode(StructuredNode):
+    def __init__(self):
+        for index, type in enumerate(neomodel_types):
+            setattr(self, "field" + str(index), type)
+
+print("+++++++++++++Schema Factory+++++++++++++++++++")
+print(SchemaFactory(TestNode()).get_schema())
+print(isinstance(SchemaFactory(TestNode()).get_schema(), graphene.ObjectType))
+print("+++++++++++++Node   Factory+++++++++++++++++++")
+print(NodeFactory(TestSchema()).get_node())
+print(isinstance(NodeFactory(TestSchema()).get_node(), neomodel.StructuredNode))
+
--- tests/test_neomodel_to_graphene_schema.py
+++ tests/test_neomodel_to_graphene_schema.py
@@ -2,6 +2,12 @@ from neographene.neographene import SchemaFactory, NodeFactory
 from tests.fixtures import TestNode, TestSchema
 
 
-def test_print():
-    assert SchemaFactory(TestNode()).__dict__ == TestSchema()
-    assert NodeFactory(TestSchema()).__dict__ == TestNode()
+def test_schema_factory():
+    schema = TestSchema()
+    node = TestNode()
+    assert SchemaFactory(node).get_schema() == schema.__dict__
+
+def test_node_factory():
+    node = TestNode()
+    schema = TestSchema()
+    assert NodeFactory(schema).get_node() == node.__dict__
